d8e64c42f2679dc4617d18a675ef27a5d067bf0a
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-17 09:53:33 -0800

    adds displayFriends() with alerts

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ addMushroomButton.addEventListener('click', () => {
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
     const newFriendName = friendInputEl.value;
-    console.log('new friends name:', newFriendName);
     // create a new friend object
     const newFriend = {
         name: newFriendName || `friend ${Math.floor(Math.random() * 500)}`,
@@ -55,20 +54,37 @@ addFriendButton.addEventListener('click', () => {
     friendInputEl.value = '';
     // clear out and display all the friends (use a function here)
     displayFriends();
+
+    console.log('new friends name:', newFriendName);
+    console.log(friendData);
+    console.log(newFriend.satisfaction);
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsEl.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
+        const friendEl = renderFriend(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
-        //         and if the friend's satisfaction level is below 3 and you have mushrooms left
-        //             increment the friends satisfaction and decrement your mushrooms
-        //             clear out and display the updated friends and mushrooms (hint: displayFriends, displayMushrooms)
+        friendEl.addEventListener('click', () => {
+            // and if the friend's satisfaction level is below 3 and you have mushrooms left
+            if (friend.satisfaction < 3 && mushroomCount > 0) {
+                // increment the friends satisfaction and decrement your mushrooms
+                friend.satisfaction++;
+                mushroomCount--;
+                // clear out and display the updated friends and mushrooms (hint: displayFriends, displayMushrooms)
+                displayFriends();
+                displayMushrooms();
+            } else if (friend.satisfaction === 3) {
+                alert('dont be greedy');
+                return;
+            } else alert('you might want to hunt for more mushrooms...');
+        });
         // append the friendEl to the friends list in DOM
+        friendsEl.append(friendEl);
     }
 }
 
